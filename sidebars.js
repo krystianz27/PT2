9b1df16ef1838bdb3e121138468b288c0102e9a0
Krystian <226311@student.uek.krakow.pl>
2023-03-13 21:31:19 +0100

    Back to jQuery again

--- sidebars.js
+++ sidebars.js
@@ -8,26 +8,26 @@
 })()
 
 
-document.getElementById("b13").addEventListener("click", myFunction);
+// document.getElementById("b13").addEventListener("click", myFunction);
 
-function myFunction() {
-  document.getElementById("img1").style.display = "none";
-  // document.getElementById("demo").src = 'k1.html';
-    document.getElementById("p1").innerHTML = " New text"
+// function myFunction() {
+//   document.getElementById("img1").style.display = "none";
+//   document.getElementById("demo").src = 'k1.html';
+//     // document.getElementById("p1").innerHTML = " New text"
 
-}
-
-
+// }
 
 
-  // $(document).ready(function(){
-  //   $("#b13").click(function(){
-  //     $("#img1").hide();
-  //     // console.log($("#p1"))
-  //     // $("#p1").load("k1.html");
-  //     // $("#p1").html("sdsa");
-  //   });
-  // });
+
+
+  $(document).ready(function(){
+    $("#b13").click(function(){
+      $("#img1").hide();
+      // console.log($("#p1"))
+      // $("#p1").load("k1.html");
+      $("#p1").html("New text");
+    });
+  });
 
 
 
